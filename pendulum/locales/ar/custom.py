c85ce2513ede68fc753c0414c22177e146529d6a
Burak Can Kahraman <brkcnkhrmn@gmail.com>
2022-04-26 09:30:26 +0300

    Add locale 'ar'.

---
+++ pendulum/locales/ar/custom.py
@@ -0,0 +1,20 @@
+"""
+ar custom locale file.
+"""
+
+translations = {
+    # Relative time
+    "ago": "منذ {time}",
+    "from_now": "من الآن {time}",
+    "after": "بعد {time}",
+    "before": "قبل {time}",
+    # Date formats
+    "date_formats": {
+        "LTS": "HH:mm:ss",
+        "LT": "HH:mm",
+        "LLLL": "dddd D MMMM YYYY HH:mm",
+        "LLL": "D MMMM YYYY HH:mm",
+        "LL": "D MMMM YYYY",
+        "L": "D/\u200FM/\u200FYYYY",
+    },
+}
